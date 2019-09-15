8abd1c7b6aa599e27f8eeeab0906d1ea6d5b5c28
munroe1786 <era3munroe@gmail.com>
2019-09-15 08:38:09 -0400

    Finalizing CLI class---adding functionality for ease of user use--adding spacing and options to move around the program.

--- lib/studio_ghibli_films/cli.rb
+++ lib/studio_ghibli_films/cli.rb
@@ -1,8 +1,9 @@
-require 'pry'
 module StudioGhibliFilms
   class CLI
       def start
+        puts ""
         puts "Hi there! Welcome to the Studio Ghibli CLI."
+        puts ""
         @input = nil
         menu
       end
@@ -17,18 +18,19 @@ module StudioGhibliFilms
           @input = gets.strip
             if @input == "menu"
               menu
-            elsif @input == "1"
+            elsif @input == "1" || @input == "list"
               list_of_films
               film_description
             elsif @input == "2"
               list_of_films
               film_director_producer_and_release_date
+            elsif @input == "exit" || @input == "quit"
+              puts "Thanks for stopping by!"
+              exit
              else
-              puts "Oops, didn't catch that! Please type '1' for list of films, '2' for film info or 'exit'."
+              puts "Oops, didn't catch that! Please type '1' for the list of films, '2' for film info or 'exit'."
             end
         end
-        puts "Thanks for stopping by!"
-        exit
       end
   
       def list_of_films
@@ -44,51 +46,67 @@ module StudioGhibliFilms
       end  
 
       def film_description
+        puts ""
+        puts "Please enter the number of a film to read its description."
         while @input != "exit" && @input != "quit" 
             @input = gets.strip
             index = @input.to_i - 1
             if index.between?(0,19)
-              #binding.pry
+              puts ""
               puts Film.all[index].title
+              puts ""
               puts Film.all[index].description
+              puts ""
               puts "Which film would you like to know more about? (type 'list' to see the list again)."
+              puts ""
               puts "Type 'menu' to return to the main menu or 'exit' to quit."
             elsif @input == "menu"
               menu
             elsif @input == "list"
               list_of_films
+              puts ""
+              puts "Please enter the number of a film to read its description."
             elsif @input == "exit" || @input == "quit"
               puts "Thanks for stopping by!"
               exit
             else
-              puts "2 Uh-oh! Please type a film number, 'list', 'menu' or 'exit'."
+              puts "Oops! Please type a film number, 'list', 'menu' or 'exit'."
           end
         end
       end
 
       def film_director_producer_and_release_date
+        puts ""
         puts "Please select a film number to see the director, producer and release date."
         while @input != "exit" && @input != "quit"
           @input = gets.strip
           index = @input.to_i - 1
           if index.between?(0,19)
+            puts ""
             puts Film.all[index].title
+            puts ""
             puts "Director: #{Film.all[index].director}"
+            puts ""
             puts "Producer: #{Film.all[index].producer}"
+            puts ""
             puts "Release Date: #{Film.all[index].release_date}"
-            puts "Which film would you like to know more about? (type 'list' to see the list again)."
-            puts "Type 'menu' to return to the main menu or 'exit' to quit."
-            puts "Would you like to see the Rotten Tomatoes scores for these films?  Press 'Y' to see the scores or 'N' to return to the menu."
+            puts ""
+            puts "Which film would you like to know more about? (type 'list', 'menu' or 'exit'."
+            puts ""
+            puts "Or would you like to see the Rotten Tomatoes scores for these films?  Press 'Y' to see the scores or 'N' to return to the menu."
           elsif @input == "list"
             list_of_films  
-          elsif @input == "menu" 
-            menu
-          elsif @input.downcase == "n"
+          elsif @input == "menu" || @input.downcase == "n"
             menu
           elsif @input.downcase == "y"
             rt_scores
+            puts ""
+            puts "Type 'list' or 'menu' to go back or 'exit' to leave the program."
+          elsif @input == "exit" || @input == "quit"
+            puts "Thanks for stopping by!"
+            exit 
           else
-            puts "3 Uh-oh! Please type a film number, 'list', 'menu' or 'exit'."
+            puts "Oops! Please type a film number, 'list', 'menu' or 'exit'."
           end  
         end  
       end
